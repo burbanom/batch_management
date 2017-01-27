944c74001faf70f6530cf3659b2f432ec1ea2cd5
Mario Burbano <burbano.carmona@gmail.com>
2017-01-27 10:33:09 +0100

    added command line arguments and check_output

--- run_md.py
+++ run_md.py
@@ -10,7 +10,7 @@ import argparse
 import matplotlib.pyplot as plt
 from os.path import isfile
 import sys
-import subprocess
+from subprocess import call, check_output, CalledProcessError
 from datetime import datetime, timedelta
 import time
 
@@ -29,9 +29,10 @@ __status__ = "Production"
 
 def parse_commandline_arguments():
     parser = argparse.ArgumentParser( description = 'Program for continously running MD simulations' )
-    parser.add_argument( '--job', '-j', metavar = 'S', type=str, required = False, help='Sample job script' )
-    #parser.add_argument( '--offset', '-so', metavar = 'N', type=int, required = True, help='Offset (in number of frames) between the slices or increment in conv. calc.' )
-    #############################################################################################################################################
+    parser.add_argument( '--job', '-j', metavar = 'S', type=str, required = True, help='Sample job script' )
+    parser.add_argument( '--hours', '-ho', metavar = 'N', type=int, required = True, help='Number of hours between after which command will be executed' )
+    parser.add_argument( '--seconds', '-s', metavar = 'N', type=int, required = True, help='Number of seconds between after which command will be executed' )
+    parser.add_argument( '--wait', '-w', metavar = 'N', type=int, required = True, help='Number of seconds that program will wait' )
     #parser.add_argument( '--msdlen', '-ml', metavar = 'N', type=int, required = True, help='Length of msd' )
     #parser.add_argument( '--prntfrq', '-p', metavar = 'N', type=int, required = False, default = 1000, help='Print frequency of displacements' )
     #parser.add_argument( '--timestep', '-t', metavar = 'F', type=float, required = False, default = 41.3414, help='Simulation timestep in a.u.' )
@@ -44,10 +45,11 @@ def parse_commandline_arguments():
 if __name__ == '__main__':
 
     args = parse_commandline_arguments()
-    interval_hours = 0
-    interval_seconds = 10
+    my_cmd = args.job
+    interval_hours = args.hours
+    interval_seconds = args.seconds
     max_loops = 1000
-    sleep_time = 2
+    sleep_time = args.wait
 
     for loop in range(max_loops):
 	
@@ -55,10 +57,11 @@ if __name__ == '__main__':
 	dt = start_time + timedelta(hours=interval_hours, seconds=interval_seconds)
 
 	while (datetime.now() < dt):
-	    print( str( loop ) + ' sleep' )
+            print( str( loop ) + ' sleep' )
 	    time.sleep(sleep_time)
 
 	else:
 	    if ( datetime.now().hour <= 17 ) and ( datetime.now().minute < 10 ):
-		print( 'else clause' )
-		print( datetime.now() )
+                cmd_output = check_output( my_cmd, shell = True ) 
+                print('else' )
+                print( cmd_output )
